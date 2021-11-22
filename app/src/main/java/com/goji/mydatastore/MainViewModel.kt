4d0d1bcfce4d60c60e69640253a2c0e7d3746a3b
panjikusumarizki <pnksm009@gmail.com>
2021-11-22 16:42:21 +0700

    Initial commit

---
+++ app/src/main/java/com/goji/mydatastore/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.goji.mydatastore
+
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val pref: SettingPreferences) : ViewModel() {
+    fun getThemeSettings(): LiveData<Boolean> {
+        return pref.getThemeSetting().asLiveData()
+    }
+
+    fun saveThemeSetting(isDarkModeActive: Boolean) {
+        viewModelScope.launch {
+            pref.saveThemeSetting(isDarkModeActive)
+        }
+    }
+}
---
+++ app/src/main/java/com/goji/mydatastore/ViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.goji.mydatastore
+
+import androidx.lifecycle.*
+
+class ViewModelFactory(private val pref: SettingPreferences) : ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(pref) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
+}
